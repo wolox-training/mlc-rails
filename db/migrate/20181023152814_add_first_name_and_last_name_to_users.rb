30319bab2974654f6277c9589798fe50978edabf
micaelacole <micaela.cole@wolox.com.ar>
2018-10-24 13:33:44 -0300

    Merged two migration files.

--- db/migrate/20181023152814_add_first_name_and_last_name_to_users.rb
+++ db/migrate/20181023152814_add_first_name_and_last_name_to_users.rb
@@ -2,5 +2,6 @@ class AddFirstNameAndLastNameToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
+    change_column_null :users, :first_name, :last_name, false
   end
 end
--- db/migrate/20181023223119_add_not_null_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNotNullToUsers < ActiveRecord::Migration[5.2]
-  def change
-  	change_column_null :users, :first_name, :last_name, false
-  end
-end
