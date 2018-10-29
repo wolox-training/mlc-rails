103a55915db01758047eaf75ddaad072d92b0bfc
micaelacole <micaela.cole@wolox.com.ar>
2018-10-29 15:08:25 -0300

    Email confirmation is now correctly skipped.

--- db/schema.rb
+++ db/schema.rb
@@ -31,19 +31,9 @@ ActiveRecord::Schema.define(version: 2018_10_26_182231) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "nickname"
-    t.string "image"
     t.json "tokens"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
