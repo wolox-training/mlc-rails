959ff8c9e2f3e57e6aa7db49138a05fc70e3d556
micaelacole <micaela.cole@wolox.com.ar>
2018-10-30 18:11:18 -0300

    Replaced image string with Faker Avatar.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    first_name { Faker::Name.first_name }
+    first_name 				{ Faker::Name.first_name }
     last_name               { Faker::Name.last_name }
     email                   { Faker::Internet.email(last_name) }
     password                { Faker::Internet.password }
