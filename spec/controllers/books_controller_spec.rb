cf5c38af9dbbc427be5607a471283c18eb14268c
micaelacole <micaela.cole@wolox.com.ar>
2018-10-31 14:30:20 -0300

    Controller test implementation.

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,4 +1,47 @@
 require 'rails_helper'
 
-RSpec.describe BooksController, type: :controller do
+describe BooksController do
+
+include_context 'Authenticated User'
+
+  describe 'GET #index' do
+    context 'When fetching all the books' do
+      let!(:books) { create_list(:book, 3) }
+
+      before do
+        get :index
+      end
+
+      it 'responses with the books json' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(
+          books, each_serializer: BookSerializer
+        ).to_json
+        expect(response.body.to_json) =~ JSON.parse(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    context 'When fetching a book' do
+      let!(:book) { create(:book) }
+
+      before do
+        get :show, params: { id: book.id }
+      end
+
+      it 'responses with the book json' do
+        expect(response.body.to_json) =~ BookSerializer.new(
+          book, root: false
+        ).to_json
+      end
+
+      it 'responds with 200 status' do
+       expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 end
