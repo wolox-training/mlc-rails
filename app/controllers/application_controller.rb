c2028a454eab2f55ccd0cb4754caa643437170aa
micaelacole <micaela.cole@wolox.com.ar>
2018-10-31 16:14:18 -0300

    Rubocop offenses corrected.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, if: :verify_api
   def verify_api
     params[:controller].split('/')[0] != 'devise_token_auth'
- end
+  end
 end
