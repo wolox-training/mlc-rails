0732130b55f562ed5131fbfcd750b9babc059737
micaelacole <micaela.cole@wolox.com.ar>
2018-11-09 15:40:11 -0300

    ActiveAdmin configured and working.

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,9 @@
+class ApiController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include Wor::Paginate
+  protect_from_forgery
+
+  def set_locale
+    I18n.locale = current_user.locale
+  end
+end
