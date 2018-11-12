21ff7b7f59f927e9349a5012008a0ca6bc05df6f
micaelacole <micaela.cole@wolox.com.ar>
2018-11-12 16:02:39 -0300

    BookSuggestion#create route implemented.

---
+++ app/controllers/book_suggestions_controller.rb
@@ -0,0 +1,18 @@
+class BookSuggestionsController < ApiController
+  before_action :set_locale
+
+  def create
+    book_suggestion = BookSuggestion.new(book_suggestion_params)
+    if book_suggestion.save
+      render json: book_suggestion, status: :created
+    else
+      render json: book_suggestion.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_suggestion_params
+    params.require(:book_suggestion).permit(:user_id, :synopsis, :price, :author, :title, :url, :publisher, :year)
+  end
+end
