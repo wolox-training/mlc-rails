330e6df5cc19a8bf5faba199ed31488fb2b22b5b
micaelacole <micaela.cole@wolox.com.ar>
2018-11-12 16:53:46 -0300

    CSRF protection added in the main application controller.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,6 @@
 class ApiController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Wor::Paginate
-  protect_from_forgery
 
   def set_locale
     I18n.locale = current_user.locale
