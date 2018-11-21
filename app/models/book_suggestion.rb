2b646180b2523296361e2e5b1a1f2ffe7d72c47a
micaelacole <micaela.cole@wolox.com.ar>
2018-11-21 16:37:20 -0300

    All tests fixed and passing again.

--- app/models/book_suggestion.rb
+++ app/models/book_suggestion.rb
@@ -1,4 +1,4 @@
 class BookSuggestion < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :author, :title, :link, :publisher, :year, presence: true
 end
--- spec/controllers/book_suggestions_controller_spec.rb
+++ spec/controllers/book_suggestions_controller_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 describe BookSuggestionsController do
   describe 'POST #create' do
     context('when user is authenticated') do
-      # include_context 'Authenticated User'
-      subject(:user) { create(:user) }
+      include_context 'Authenticated User'
 
       context 'and creating a valid book suggestion' do
-        subject(:book_s) { build(:book_suggestion, user: user) }
+        let(:book_s) { build(:book_suggestion, user: user) }
         let(:http_request) { post :create, params: { book_suggestion: book_s.attributes } }
 
         it 'creates a new book suggestion' do
@@ -26,7 +25,7 @@ describe BookSuggestionsController do
       end
 
       context 'and creating an invalid book suggestion' do
-        subject(:book_s) { build(:book_suggestion, user: nil) }
+        let(:book_s) { build(:book_suggestion, user: user, author: nil) }
         let(:http_request) { post :create, params: { book_suggestion: book_s.attributes } }
 
         it 'doesn\'t create a new book suggestion' do
@@ -42,7 +41,7 @@ describe BookSuggestionsController do
 
     context 'when user is not authenticated' do
       context 'and creating a valid book suggestion' do
-        subject(:book_s) { build(:book_suggestion) }
+        let(:book_s) { build(:book_suggestion, user: nil) }
         let(:http_request) { post :create, params: { book_suggestion: book_s.attributes } }
 
         it 'creates a new book suggestion' do
@@ -54,14 +53,14 @@ describe BookSuggestionsController do
           expect(response).to have_http_status(:created)
         end
 
-        # it 'sets user id to nil' do
-        #   http_request
-        #   expect(response_body['user_id']).to be_nil
-        # end
+        it 'sets user id to nil' do
+          http_request
+          expect(response_body['user_id']).to be_nil
+        end
       end
 
       context 'and creating an invalid book suggestion' do
-        subject(:book_s) { build(:book_suggestion, user: nil) }
+        let(:book_s) { build(:book_suggestion, user: nil, author: nil) }
         let(:http_request) { post :create, params: { book_suggestion: book_s.attributes } }
 
         it 'doesn\'t create a new book suggestion' do
